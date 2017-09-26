6bf6b3bf86b893afb7f4c7141f740f83952a2312
Chun Hau Lai <c.lai@adevcom.cl>
2017-09-26 01:32:03 -0300

    Agregar discretizacion del precio

--- src/main/java/cl/lai/datamining/App_listar_reglas_asociacion_dpto.java
+++ src/main/java/cl/lai/datamining/App_listar_reglas_asociacion_dpto.java
@@ -16,11 +16,12 @@ import java.util.TreeMap;
 import javax.smartcardio.ATR;
 
 import weka.associations.Apriori;
+import weka.clusterers.SimpleKMeans;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instances;
 import weka.filters.Filter;
-import weka.filters.unsupervised.attribute.NumericToNominal;
+import weka.filters.unsupervised.attribute.Discretize;
 
 /**
  * Hello world!
@@ -28,17 +29,79 @@ import weka.filters.unsupervised.attribute.NumericToNominal;
  */
 public class App_listar_reglas_asociacion_dpto 
 {
-    public static void main( String[] args ) throws IOException{
+    public static void main( String[] args ) throws Exception{
     	HashMap<String, String> tipoProducto = new HashMap<String, String>();
     	BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("/Users/chunhaulai/Documents/workspace/lai-datamining-app/src/resources/arriendo_dpto_categoria.csv"))));
     	BufferedReader readerSoloGrupos = new BufferedReader(new InputStreamReader(new FileInputStream(new File("/Users/chunhaulai/Documents/workspace/lai-datamining-app/src/resources/arriendo_dpto_categoria.csv"))));
-   	 
+    	
+    	//dividir el grupo de precios
+    	BufferedReader leerPrecios = new BufferedReader(new InputStreamReader(new FileInputStream(new File("/Users/chunhaulai/Documents/workspace/lai-datamining-app/src/resources/arriendo_dpto_categoria.csv"))));    	
+    	List<Integer> precios = new ArrayList<Integer>();
     	String aux = null;
+    	aux=leerPrecios.readLine();
+    	while((aux=leerPrecios.readLine())!=null){
+    		String grupos[] = aux.split(";");
+ 			int precio = Integer.parseInt(grupos[1]);
+			precios.add(precio);
+    	}
+    	
+    	Attribute precioAttr = new Attribute("precio");
+    	ArrayList<Attribute> precioAttrs = new ArrayList<Attribute>();
+    	precioAttrs.add(precioAttr);
+    	Instances precios_todos = new Instances("precios", precioAttrs, 0);
+    	for(int precio: precios){
+    		DenseInstance inst_precio = new DenseInstance(1);
+    		inst_precio.setValue(precioAttr, precio);
+    		precios_todos.add(inst_precio);
+    	}
+    	
+    	
+    	SimpleKMeans kMeans = new SimpleKMeans();
+        kMeans.setNumClusters(10);
+        kMeans.buildClusterer(precios_todos); 
+    	 
+        
+        Map<Integer,  Integer[]> rangos = new HashMap<Integer,  Integer[]>();
+        for (int i = 0; i < precios_todos.numInstances(); i++) { 
+        	int grupo = kMeans.clusterInstance(precios_todos.instance(i));
+        	int valor = 	(int) precios_todos.instance(i).value(0);
+            System.out.println( precios_todos.instance(i) + " is in cluster " + kMeans.clusterInstance(precios_todos.instance(i)) ); 
+            if(!rangos.containsKey( grupo)){
+            	rangos.put(grupo, new Integer[]{
+            			valor,
+            			valor});
+            }else{
+            	int min = rangos.get(grupo)[0];
+            	int max = rangos.get(grupo)[1];
+            	if(valor < min)
+            		 rangos.get(grupo)[0] = valor;
+            	if(valor > max)
+            		 rangos.get(grupo)[1] = valor;
+            }
+        } 
+        
+        Map<Integer,String> valorNominal = new HashMap<Integer,String>();
+        for (int i = 0; i < precios_todos.numInstances(); i++) { 
+        	int grupo = kMeans.clusterInstance(precios_todos.instance(i));
+        	int valor = 	(int) precios_todos.instance(i).value(0);
+        	int min = rangos.get(grupo)[0];
+        	int max = rangos.get(grupo)[1];
+        	valorNominal.put(valor, min+"~"+max);
+        }
+       // System.out.println(rangos.get(1)[0]+"=>"+rangos.get(1)[1]);
+        //System.exit(1);
+        
+        
+        
+        
+    	
+    	
     	Instances dptos = null;
     	ArrayList<Attribute> attributes = null;
     	int count = 0;
      	List<HashSet<String>> my_nominal_grupos = new ArrayList<HashSet<String>>( ); 
     	int filas = 0;
+    	
     	while((aux=readerSoloGrupos.readLine())!=null){
     		String grupos[] = aux.split(";");
     		if(filas==0){
@@ -53,37 +116,29 @@ public class App_listar_reglas_asociacion_dpto
     			int columna =0;
      			for(String g: grupos){
      				if(columna==1){
-     					Integer precio = Integer.parseInt(g);
-     					if(precio < 500){
-     						my_nominal_grupos.get(columna).add(" < 500 U.F.");
-     					}else if(precio < 1000)
-     						my_nominal_grupos.get(columna).add(" 501 - 1000 U.F.");
-     					else if(precio < 1500)
-     						my_nominal_grupos.get(columna).add(" 1001 - 1500 U.F.");
-     					else if(precio < 2000) 
-     						my_nominal_grupos.get(columna).add(" 1501 - 2000 U.F.");
-     					else if(precio < 3000) 
-     						my_nominal_grupos.get(columna).add(" 2001 - 3000 U.F.");
-     					else  if(precio < 4000) 
-     						my_nominal_grupos.get(columna).add(" 3001 - 4000 U.F.");
-     					else 
-     						my_nominal_grupos.get(columna).add(" > 4000 U.F.");
+     					int precio = Integer.parseInt(g);
+     					my_nominal_grupos.get(columna).add(valorNominal.get(precio));
      					
      				}else 
-     				my_nominal_grupos.get(columna).add(g);
+     					my_nominal_grupos.get(columna).add(g);
     				columna++;
     			}
     		}
     		filas++;
     	}
+    	
+    	
+    	
+        
     	filas = 0;
       	 while((aux=reader.readLine())!=null){
      		String grupos[] = aux.split(";");
     		if(filas==0){
     			attributes = new ArrayList<Attribute>(grupos.length-1);
         		for(int i=1;i<grupos.length;i++){
-        			Attribute attr = new Attribute(grupos[i],new ArrayList<String>(my_nominal_grupos.get(i)));
+    				Attribute attr = new Attribute(grupos[i],new ArrayList<String>(my_nominal_grupos.get(i)));
         			attributes.add(attr);
+        			
         		}
         		dptos = new Instances("dptos", attributes, 0);
     		}else{
@@ -95,21 +150,8 @@ public class App_listar_reglas_asociacion_dpto
     				if( (">400".equalsIgnoreCase(grupos[i]) )){
     					inst.setMissing(attributes.get(i-1 ));
     				}else if(i==1){
-    					Integer precio = Integer.parseInt(grupos[i]);
-     					if(precio < 500){
-     						inst.setValue(attributes.get(i-1 )," < 500 U.F.");
-     					}else if(precio < 1000)
-     						inst.setValue(attributes.get(i-1 )," 501 - 1000 U.F.");
-     					else if(precio < 1500)
-     						inst.setValue(attributes.get(i-1 )," 1001 - 1500 U.F.");
-     					else if(precio < 2000) 
-     						inst.setValue(attributes.get(i-1 )," 1501 - 2000 U.F.");
-     					else if(precio < 3000) 
-     						inst.setValue(attributes.get(i-1 )," 2001 - 3000 U.F.");
-     					else if(precio < 4000) 	
-     						inst.setValue(attributes.get(i-1 )," 3001 - 4000 U.F.");
-     					else 
-     						inst.setValue(attributes.get(i-1 )," > 4000 U.F.");
+    					int precio = Integer.parseInt(grupos[i]);
+     					inst.setValue(attributes.get(i-1 ),valorNominal.get(precio));
      				}else 
     					inst.setValue(attributes.get(i-1 ),grupos[i]);
         		}
@@ -118,10 +160,11 @@ public class App_listar_reglas_asociacion_dpto
     		filas++;
     		 
     	}
+ 
      	Apriori aprioriObj = new Apriori();
     	try {
     		//minimo soporte 0.1, minima confianza 0.5, cantidad de reglas: 30
-    		String []options =  {"-C","0.5","-N","30","-M","0.05"};
+    		String []options =  {"-C","0.8","-N","30","-M","0.05"};
     		aprioriObj.setOptions(options);
     		aprioriObj.buildAssociations(dptos);
     	} catch (Exception e) {
